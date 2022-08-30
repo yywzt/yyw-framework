6bf5fae2c8537051952815a4bc20e202c1c16d58
严志涛 <yanzhitao@xiaomalixing.com>
2022-08-30 10:41:03 +0800

    enhancement: BusinessException继承RuntimeException，增加AppCode参数的构造方法

--- framework-api/src/main/java/com/yyw/api/exception/BusinessException.java
+++ framework-api/src/main/java/com/yyw/api/exception/BusinessException.java
@@ -1,10 +1,14 @@
 package com.yyw.api.exception;
 
+import com.yyw.api.model.AppCode;
+
+import java.text.MessageFormat;
+
 /**
  * @author yanzhitao@xiaomalixing.com
  * @date 2021/4/14 18:28
  */
-public class BusinessException extends Exception {
+public class BusinessException extends RuntimeException {
     private String code = "";
     private String message = "";
 
@@ -23,6 +27,22 @@ public class BusinessException extends Exception {
         super(message, cause);
         this.code = code;
         this.message = message;
+    }
+
+    public BusinessException(AppCode appCode) {
+        this(appCode.getCode(), appCode.getMessage());
+    }
+
+    public BusinessException(AppCode appCode, Throwable cause) {
+        this(appCode.getCode(), appCode.getMessage(), cause);
+    }
+
+    public BusinessException(AppCode appCode, Object... parameter) {
+        this(appCode.getCode(), MessageFormat.format(appCode.getMessage(), parameter));
+    }
+
+    public BusinessException(AppCode appCode, Throwable cause, Object... parameter) {
+        this(appCode.getCode(), MessageFormat.format(appCode.getMessage(), parameter), cause);
     }
 
     public String getCode() {
